c4879d33f30284b1acf7f2da0b8389a999330bc7
travins <etin@qq.com>
2018-04-08 10:46:56 +0800

    add nodemon ande validate JOI

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
 module.exports = {
     signUp: async (req, res, next) => {
+        //Email & password
+        console.log('req.value.body',req.value.body)
         console.log('UsersController.signUp() called!')
     },
 
     signIn: async (req, res, next) => {
+        //Generate token
         console.log('UsersController.signIn() called!')      
     },
 
